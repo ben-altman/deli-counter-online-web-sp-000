b8356d30dc6064e46dad3c2cbffa0d93672e132f
Ben Altman <altmanguitar@gmail.com>
2020-11-10 14:34:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,9 @@ def line(array)
   customers = []
   if array.length >= 1
     array.each_with_index do |name, index|
-    customers.push ("#{index + 1}. #{name}")
+      customers.push ("#{index + 1}. #{name}")
     end
     puts "The line is currently: #{customers.join}"
-    end
   else
     puts "The line is currently empty."
   end
