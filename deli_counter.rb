692b74cd11664932663e29a6fb935017a1912d5e
Ben Altman <altmanguitar@gmail.com>
2020-11-10 14:22:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(array)
   if array.length >= 1
     array.each_with_index do |value, index|
     puts "The line is currently: #{index}. #{name}"
+  end
   else
     puts "The line is currently empty."
   end
