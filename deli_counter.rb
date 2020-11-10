eec994d95e42bf08686deae8f4ce610b3cd46a76
Ben Altman <altmanguitar@gmail.com>
2020-11-10 14:23:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(array)
   customers = []
   if array.length >= 1
-    array.each_with_index do |value, index|
+    array.each_with_index do |name, index|
     puts "The line is currently: #{index}. #{name}"
     end
   else
