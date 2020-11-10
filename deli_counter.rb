d1bd611ab06ea6f88d92ae7497037512882b0409
Ben Altman <altmanguitar@gmail.com>
2020-11-10 14:20:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ katz_deli = []
 
 def line(array)
   #customers = []
-  if array.length >= 1
-    array.each_with_index do |value, index|
-    puts "The line is currently: #{index}. #{name}"
-  else
-    puts "The line is currently empty."
-  end
+#  if array.length >= 1
+#    array.each_with_index do |value, index|
+#    puts "The line is currently: #{index}. #{name}"
+#  else
+#    puts "The line is currently empty."
+#  end
 end
