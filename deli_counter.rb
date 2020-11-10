46f8160a98de2d5b3f0e704e95da3caff3684012
Ben Altman <altmanguitar@gmail.com>
2020-11-10 14:45:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,15 @@ katz_deli = []
 def line(array)
   customers = []
   if array.length >= 1
-    array.each_with_index do |name, index|
-      customers.push ("#{index + 1}. #{name}")
+    array.each_with_index(2) do |name, index|
+      customers.push ("#{index}. #{name}")
     end
     puts "The line is currently: #{customers.join(" ")}"
   else
     puts "The line is currently empty."
   end
+end
+
+def take_a_number(array, name)
+  
 end
